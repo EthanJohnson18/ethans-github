d01d55b45651107ff7370c5596e5144e6a1af24f
Davis T <107003461+davist35@users.noreply.github.com>
2023-03-03 10:18:17 -0600

    Added Sound

--- main.ts
+++ main.ts
@@ -56,3 +56,4 @@ let mySprite = sprites.create(img`
     ddddddddddddddddddddddbbbdddddddddddddddddddddddddddddddddddddd
     ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
     `, SpriteKind.Player)
+music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
